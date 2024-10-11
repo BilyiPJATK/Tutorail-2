6cdd1c458d8cf6f7a7db24e271b1bc6eccdf5899
BilyiPJATK <s31718@pjwstk.edu.pl>
2024-10-11 16:56:29 +0200

    Specified who adds `Adder` and who adds `Subtractor` classes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
  public static void main(String[] args) {
   Adder adder = new Adder();
-  System.out.println(adder.add(4, 6));
+  System.out.println(adder.add(5, 1));
 
   Subtractor subtractor = new Subtractor();
   System.out.println(subtractor.subtract(8, 10));
