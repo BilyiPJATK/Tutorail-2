eb34e19839eeaed867facf2e2e28dfc2d282e708
jamiqq <s30879@pjwstk.edu.pl>
2024-10-11 16:58:36 +0200

    Modifying subtract method

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
   System.out.println(adder.add(5, 1));
 
   Subtractor subtractor = new Subtractor();
-  System.out.println(subtractor.subtract(2, 10));
+  System.out.println(subtractor.subtract(2, 100));
  }
 }
